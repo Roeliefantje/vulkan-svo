8a1674e43162ce6ce0a77529f75558b8801c5e09
Roeliefantje <roel.de.jeu@gmail.com>
2025-11-08 19:40:55 +0100

    fix: tribox file

--- include/tribox.h
+++ include/tribox.h
@@ -119,25 +119,12 @@ int planeBoxOverlap(float normal[3], float d, float maxbox[3]) {
 	if(min>rad || max<-rad) return 0;
 
 int triBoxOverlap(float boxcenter[3], float boxhalfsize[3], float triverts[3][3]) {
-	/*    use separating axis theorem to test overlap between triangle and box */
-	/*    need to test for overlap in these directions: */
-	/*    1) the {x,y,z}-directions (actually, since we use the AABB of the triangle */
-	/*       we do not even need to test these) */
-	/*    2) normal of the triangle */
-	/*    3) crossproduct(edge from tri, {x,y,z}-directin) */
-	/*       this gives 3x3=9 more tests */
 	float v0[3], v1[3], v2[3];
 	float axis[3];
 	float min, max, d, p0, p1, p2, rad, fex, fey, fez;
 	float normal[3], e0[3], e1[3], e2[3];
 
-	/* 1) first test overlap in the {x,y,z}-directions */
-	/*    find min, max of the triangle each direction, and test for overlap in */
-	/*    that direction -- this is equivalent to testing a minimal AABB around */
-	/*    the triangle against the AABB */
 #if 1
-	/* This is the fastest branch on Sun */
-	/* move everything so that the boxcenter is in (0,0,0) */
 	SUB(v0, triverts[0], boxcenter);
 	SUB(v1, triverts[1], boxcenter);
 	SUB(v2, triverts[2], boxcenter);
@@ -154,43 +141,34 @@ int triBoxOverlap(float boxcenter[3], float boxhalfsize[3], float triverts[3][3]
 	FINDMINMAX(v0[Z_AXIS], v1[Z_AXIS], v2[Z_AXIS], min, max);
 	if (min > boxhalfsize[Z_AXIS] || max < -boxhalfsize[Z_AXIS]) return 0;
 #else
-   /*    another implementation */
-   /*    test in X */
-   v0[X]=triverts[0][X]-boxcenter[X];
-   v1[X]=triverts[1][X]-boxcenter[X];
-   v2[X]=triverts[2][X]-boxcenter[X];
-   FINDMINMAX(v0[X],v1[X],v2[X],min,max);
-   if(min>boxhalfsize[X] || max<-boxhalfsize[X]) return 0;
+	v0[X_AXIS]=triverts[0][X_AXIS]-boxcenter[X_AXIS];
+	v1[X_AXIS]=triverts[1][X_AXIS]-boxcenter[X_AXIS];
+	v2[X_AXIS]=triverts[2][X_AXIS]-boxcenter[X_AXIS];
+	FINDMINMAX(v0[X_AXIS],v1[X_AXIS],v2[X_AXIS],min,max);
+	if(min>boxhalfsize[X_AXIS] || max<-boxhalfsize[X_AXIS]) return 0;
 
-   /*    test in Y */
-   v0[Y]=triverts[0][Y]-boxcenter[Y];
-   v1[Y]=triverts[1][Y]-boxcenter[Y];
-   v2[Y]=triverts[2][Y]-boxcenter[Y];
-   FINDMINMAX(v0[Y],v1[Y],v2[Y],min,max);
-   if(min>boxhalfsize[Y] || max<-boxhalfsize[Y]) return 0;
+	v0[Y_AXIS]=triverts[0][Y_AXIS]-boxcenter[Y_AXIS];
+	v1[Y_AXIS]=triverts[1][Y_AXIS]-boxcenter[Y_AXIS];
+	v2[Y_AXIS]=triverts[2][Y_AXIS]-boxcenter[Y_AXIS];
+	FINDMINMAX(v0[Y_AXIS],v1[Y_AXIS],v2[Y_AXIS],min,max);
+	if(min>boxhalfsize[Y_AXIS] || max<-boxhalfsize[Y_AXIS]) return 0;
 
-   /*    test in Z */
-   v0[Z]=triverts[0][Z]-boxcenter[Z];
-   v1[Z]=triverts[1][Z]-boxcenter[Z];
-   v2[Z]=triverts[2][Z]-boxcenter[Z];
-   FINDMINMAX(v0[Z],v1[Z],v2[Z],min,max);
-   if(min>boxhalfsize[Z] || max<-boxhalfsize[Z]) return 0;
+	v0[Z_AXIS]=triverts[0][Z_AXIS]-boxcenter[Z_AXIS];
+	v1[Z_AXIS]=triverts[1][Z_AXIS]-boxcenter[Z_AXIS];
+	v2[Z_AXIS]=triverts[2][Z_AXIS]-boxcenter[Z_AXIS];
+	FINDMINMAX(v0[Z_AXIS],v1[Z_AXIS],v2[Z_AXIS],min,max);
+	if(min>boxhalfsize[Z_AXIS] || max<-boxhalfsize[Z_AXIS]) return 0;
 #endif
 
-	/*    2) */
-	/*    test if the box intersects the plane of the triangle */
-	/*    compute plane equation of triangle: normal*x+d=0 */
-	SUB(e0, v1, v0); /* tri edge 0 */
-	SUB(e1, v2, v1); /* tri edge 1 */
+	SUB(e0, v1, v0);
+	SUB(e1, v2, v1);
 	CROSS(normal, e0, e1);
-	d = -DOT(normal, v0); /* plane eq: normal.x+d=0 */
+	d = -DOT(normal, v0);
 
 	if (!planeBoxOverlap(normal, d, boxhalfsize)) return 0;
 
-	/*    compute the last triangle edge */
 	SUB(e2, v0, v2);
 
-	/*    3) */
 	fex = fabs(e0[X_AXIS]);
 	fey = fabs(e0[Y_AXIS]);
 	fez = fabs(e0[Z_AXIS]);
@@ -204,7 +182,6 @@ int triBoxOverlap(float boxcenter[3], float boxhalfsize[3], float triverts[3][3]
 	AXISTEST_X01(e1[Z_AXIS], e1[Y_AXIS], fez, fey);
 	AXISTEST_Y02(e1[Z_AXIS], e1[X_AXIS], fez, fex);
 	AXISTEST_Z0(e1[Y_AXIS], e1[X_AXIS], fey, fex);
-
 
 	fex = fabs(e2[X_AXIS]);
 	fey = fabs(e2[Y_AXIS]);
